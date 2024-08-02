0c45feddbcff2cd614aa55762ee49cfa050409cb
Souza <diogo-romano.souza@capgemini.com>
2024-08-02 14:43:53 +0100

    IMPROVE: Added Service, Controller and Config settings

---
+++ src/main/java/com/capgemini/genai/clinicaldecision/model/Patient.java
@@ -0,0 +1,25 @@
+package com.capgemini.genai.clinicaldecision.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "patient")
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "document")
+    private String document;
+
+}
